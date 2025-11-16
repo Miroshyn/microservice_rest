ba2594507d673123afff5753fa5604435b2b739d
Mykyta <miroshina999@gmail.com>
2025-11-16 12:52:41 +0100

    Update test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,23 +1,20 @@
 import pytest
 import httpx
 
-BASE_URL = "http://localhost:8003/orders"
-AUTH_URL = "http://localhost:8001"
+BASE_URL = "http://order-service:8003/orders"
+AUTH_URL = "http://auth-service:8001"
 
-TOKEN = "Bearer fake-token-for-alice@example.com"
+USER = {"email": "alice@example.com", "password": "alice123"}
 
 @pytest.mark.asyncio
 async def test_create_order_insufficient_stock():
     async with httpx.AsyncClient() as client:
-        # Логін через GET
-        login_resp = await client.get(f"{AUTH_URL}/login", params={
-            "email": "alice@example.com",
-            "password": "alice123"
-        })
+        # Отримати токен
+        login_resp = await client.get(f"{AUTH_URL}/login", params=USER)
         assert login_resp.status_code == 200, f"Login failed: {login_resp.text}"
         token = login_resp.json()["accessToken"]
 
-        # Створення замовлення з кількістю більше за наявну
+        # Створення замовлення з qty > inStock
         r = await client.post(
             BASE_URL,
             json={"productId": 101, "qty": 100},
